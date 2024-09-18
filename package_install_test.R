61940b441cd253b756581c649b91157f4d4af5e0
Daisuke Ozaki <daisukeozaki@gmail.com>
2024-09-18 22:25:34 +0900

    Update package_install_test.R

--- package_install_test.R
+++ package_install_test.R
@@ -1,3 +1,10 @@
 #package_install_test
 
+# remotesパッケージのインストール
+install.packages("remotes")
+
+# 本レポジトリに必要な従属パッケージのインストール
+remotes::install_deps()
+
+# 本レポジトリをパッケージとしてインストール
 remotes::install_github("keisemi/EconometriciansGuide_CausalInference")
